854e20271ba27aaec17367d11da327578e564bf6
Klaus Steinmeier <klaus.steinmeier@sap.com>
2023-02-10 17:11:03 -0300

    Start JS logic

---
+++ app.js
@@ -0,0 +1,9 @@
+const STANDARD_GRID_WIDTH = 16;
+const STANDARD_GRID_HEIGHT = 16;
+function createGrid() {
+    for(let i = 0; i < STANDARD_GRID_HEIGHT; i++) {
+        for (let j = 0; j < STANDARD_GRID_WIDTH; j++) {
+            let gridCell = document.createElement('div')
+        }
+    }
+}
