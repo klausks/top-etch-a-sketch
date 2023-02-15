d8306452c82589c81a43a5579a0e71e747177e18
Klaus Steinmeier <klaus.steinmeier@sap.com>
2023-02-15 09:21:22 -0300

    Implement drawing

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const STANDARD_GRID_WIDTH = 16;
 const STANDARD_GRID_HEIGHT = 16;
+
+const GRID = document.querySelector('.grid');
 function createGrid() {
     for(let i = 0; i < STANDARD_GRID_HEIGHT; i++) {
         for (let j = 0; j < STANDARD_GRID_WIDTH; j++) {
             let gridCell = document.createElement('div')
+            gridCell.classList.add('grid-cell');
+            GRID.appendChild(gridCell);
         }
     }
 }
+
+createGrid();
+
+GRID.addEventListener('mouseover', e => {
+    e.target.classList.add('hovered');
+})
+
