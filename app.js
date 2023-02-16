fe980e512c913888643f70fb7eeedf85bdbb6f91
Klaus Steinmeier <klaus.steinmeier@sap.com>
2023-02-16 09:59:18 -0300

    MVP

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-const STANDARD_GRID_WIDTH = 16;
-const STANDARD_GRID_HEIGHT = 16;
-
+const STANDARD_GRID_SIZE = 16;
 const GRID = document.querySelector('.grid');
-function createGrid() {
-    for(let i = 0; i < STANDARD_GRID_HEIGHT; i++) {
-        for (let j = 0; j < STANDARD_GRID_WIDTH; j++) {
+const ROOT = document.querySelector(':root');
+
+function createGrid(size) {
+    clearGrid();
+    resizeGrid(size);
+    for(let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             let gridCell = document.createElement('div')
             gridCell.classList.add('grid-cell');
             GRID.appendChild(gridCell);
         }
     }
+
+    GRID.addEventListener('mouseover', e => {
+        if (e.target.classList.contains('grid-cell')) {
+            e.target.classList.add('hovered');
+        }
+        e.stopPropagation();
+    });
 }
 
-createGrid();
+function clearGrid() {
+    GRID.innerHTML = "";
+}
 
-GRID.addEventListener('mouseover', e => {
-    e.target.classList.add('hovered');
+function resizeGrid(size) {
+    GRID.style.setProperty('--grid-cols', size);
+    GRID.style.setProperty('--grid-rows', size);
+}
+
+createGrid(STANDARD_GRID_SIZE);
+
+let resetGridBtn = document.querySelector('button.reset-grid');
+resetGridBtn.addEventListener('click', () => {
+    let gridSize = Number.parseInt(
+        prompt("Please inform the desired grid size (up to 100)")
+    );
+    if (isNaN(gridSize)) {
+        alert('Invalid input. Only integers are accepted.');
+        return;
+    }
+    if (gridSize > 100 || gridSize < 1) {
+        alert('Invalid input. The size should be between 1 and 100');
+        return;
+    }
+
+    createGrid(gridSize);
 })
 
